06c083d705d542e71f99211bc9dfa55ef6e5da26
kshwork1 <61817152+kshwork1@users.noreply.github.com>
2020-04-13 22:09:40 +0900

    last

--- 5w/5w_new/5th_memo.py
+++ 5w/5w_new/5th_memo.py
@@ -1,41 +1,31 @@
 from flask import Flask, render_template, jsonify, request
-
 app = Flask(__name__)
 
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+import requests
+from bs4 import BeautifulSoup
 
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
-
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
+@app.route('/memo', methods=['GET'])
+def listing():
+    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
+    # 2. articles라는 키 값으로 영화정보 내려주기
+    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!'})
 
 ## API 역할을 하는 부분
-@app.route('/reviews', methods=['POST'])
-def write_review():
-    ## requestform > 데이터 받아오는 것
-    title_receive = request.form['title_give']
-    author_receive = request.form['author_give']
-    review_receive = request.form['review_give']
-
-    db.reviews.insert_one({
-        "title": title_receive,
-        "author": author_receive,
-        "review": review_receive
-    })
-
-@app.route('/reviews', methods=['GET'])
-def read_reviews():
-    # 1. 모든 reviews의 문서를 가져온 후 list로 변환합니다.
-    # 2. 성공 메시지와 함께 리뷰를 보냅니다.
-    reviews = list(db.reviews.find({}, {'_id': False}))
-
-    return jsonify({'result': 'success', 'msg': '이 요청은 GET!', 'reviews': reviews})
-
+@app.route('/memo', methods=['POST'])
+def saving():
+		# 1. 클라이언트로부터 데이터를 받기
+		# 2. meta tag를 스크래핑하기
+		# 3. mongoDB에 데이터 넣기
+    return jsonify({'result': 'success', 'msg':'POST 연결되었습니다!'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0',port=5000,debug=True)
